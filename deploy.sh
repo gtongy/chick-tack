1b5a4748a187426bd0eeaddb44ca50bee47104e8
gtongy <32930281+gtongy@users.noreply.github.com>
2018-05-29 23:56:34 +0900

    travis ci include

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Install Hugo
+wget https://github.com/gohugoio/hugo/releases/download/v0.37/hugo_0.37_Linux-64bit.tar.gz
+tar -xzf hugo_0.37_Linux-64bit.tar.gz
+sudo mv hugo /usr/local/bin/
+
+# deploy github pages
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 hugo -t "hugo-theme-cactus-plus"
 cd public
@@ -9,5 +15,5 @@ if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
-git push origin gh-pages
+git push -f origin gh-pages
 cd ..
