95cfb2bea37052e5eb9f55c597870cb9caac669e
gtongy <32930281+gtongy@users.noreply.github.com>
2018-06-02 22:08:20 +0900

    shell change

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,13 @@
 # deploy github pages
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 cd public
-git add .
+GIT_DIFF = `git diff --name-only`
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
-git commit -m "$msg"
+if [ $GIT_DIFF ]; then
+  git add .
+  git commit -m "$msg"
+fi
 git push -f origin gh-pages
