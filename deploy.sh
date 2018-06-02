7193a5a2825edd836b41f7b06c8c49e21a2aad07
gtongy <32930281+gtongy@users.noreply.github.com>
2018-06-03 02:22:59 +0900

    api token push

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone git@github.com:gtongy/chick-tack.git
+git clone --quiet https://$GH_TOKEN@github.com/gtongy/chick-tack.git
 cd chick-tack
 git clone https://github.com/nodejh/hugo-theme-cactus-plus.git ./themes/hugo-theme-cactus-plus
 # deploy github pages message
@@ -15,12 +15,12 @@ echo "DIST_DIR : ${DIST_DIR}"
 rm -rf ${DIST_DIR}/* || exit 0
 ./binaries/hugo -t "hugo-theme-cactus-plus"
 cd ${DIST_DIR}
-git init
 git add -A .
 echo ${msg}
 git commit -m "${msg}"
+echo "${msg}"
 
 # GitHubにpush
 echo "Push to GitHub"
-git push origin gh-pages > /dev/null 2>&1
+git push --quiet https://$GH_TOKEN@github.com/gtongy/chick-tack.git gh-pages > /dev/null 2>&1
 echo "Successfully deployed."
