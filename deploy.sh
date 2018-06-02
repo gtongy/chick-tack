038ce10a75c1afee3969469728f2091a35319ac2
gtongy <32930281+gtongy@users.noreply.github.com>
2018-06-02 22:29:54 +0900

    fetch latest master

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+# deploy github pages message
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+
+# サイトを生成するディレクトリ
 DIST_DIR=./public
 echo "DIST_DIR : ${DIST_DIR}"
+
+# 変更があったらcommit
 cd ${DIST_DIR}
 git add .
 d=`date +"%Y/%m/%d %k:%M:%S"`
-git diff --cached --exit-code --quiet || git commit -m "$msg"
+git diff --cached --exit-code --quiet || git commit -m "${msg}"
+
+# GitHubにpush
 echo "Push to GitHub"
 git push origin gh-pages > /dev/null 2>&1
 echo "Successfully deployed."
