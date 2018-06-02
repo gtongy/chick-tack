aca43e2131e587c30d7444fba2934a285480e1b3
gtongy <32930281+gtongy@users.noreply.github.com>
2018-06-02 22:12:04 +0900

    if add file exist, commit

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,10 @@
 # deploy github pages
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 cd public
-GIT_DIFF = `git diff --name-only`
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
-if [ $GIT_DIFF ]; then
-  git add .
-  git commit -m "$msg"
-fi
+git add .
+git diff --cached --exit-code --quiet || git commit -m "$msg"
 git push -f origin gh-pages
