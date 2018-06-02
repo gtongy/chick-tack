cd37529434a7f38c50be68c5886f3df847df38da
gtongy <32930281+gtongy@users.noreply.github.com>
2018-06-03 02:42:06 +0900

    work tree add, change origin

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@
 git clone --quiet https://$GH_TOKEN@github.com/gtongy/chick-tack.git
 cd chick-tack
 git clone https://github.com/nodejh/hugo-theme-cactus-plus.git ./themes/hugo-theme-cactus-plus
+git fetch origin gh-pages:remotes/origin/gh-pages
+git worktree add -B gh-pages public origin/gh-pages
 # deploy github pages message
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
@@ -15,14 +17,12 @@ echo "DIST_DIR : ${DIST_DIR}"
 rm -rf ${DIST_DIR}/* || exit 0
 ./binaries/hugo -t "hugo-theme-cactus-plus"
 cd ${DIST_DIR}
-git init
-git checkout -b gh-pages
-git add -A .
+git add --all
 echo ${msg}
 git commit -m "${msg}"
 echo "${msg}"
 
 # GitHubにpush
 echo "Push to GitHub"
-git push --quiet --set-upstream https://$GH_TOKEN@github.com/gtongy/chick-tack.git gh-pages > /dev/null 2>&1
+git push --quiet origin gh-pages > /dev/null 2>&1
 echo "Successfully deployed."
